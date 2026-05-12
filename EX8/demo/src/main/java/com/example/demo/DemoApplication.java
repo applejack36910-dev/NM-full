54a1d164576f75b70fc29b3bce551339de602cf6
applejack36910-dev <applejack36910@gmail.com>
2026-05-12 21:34:48 +0530

    Update DemoApplication.java

--- EX8/demo/src/main/java/com/example/demo/DemoApplication.java
+++ EX8/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -20,3 +20,26 @@ public class DemoApplication {
         };
     }
 }
+
+/*CREATE
+POST http://localhost:8080/products
+Body → raw → JSON:
+{
+    "name": "Keyboard",
+    "price": 89.99,
+    "category": "Electronics"
+}
+READ ALL
+GET http://localhost:8080/products
+READ ONE
+GET http://localhost:8080/products/1
+UPDATE
+PUT http://localhost:8080/products/1
+Body → raw → JSON:
+{
+    "name": "Gaming Laptop",
+    "price": 1299.99,
+    "category": "Electronics"
+}
+DELETE
+DELETE http://localhost:8080/products/1*/
