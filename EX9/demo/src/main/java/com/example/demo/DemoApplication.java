913353d1cf84686938ef6a9c94d628cea6aba458
applejack36910-dev <applejack36910@gmail.com>
2026-05-12 21:36:23 +0530

    Update DemoApplication.java

--- EX9/demo/src/main/java/com/example/demo/DemoApplication.java
+++ EX9/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -26,3 +26,25 @@ public class DemoApplication {
         };
     }
 }
+
+/*Test in Postman
+Flights
+GET  /flights
+GET  /flights/1
+GET  /flights/search?source=Chennai&destination=Mumbai
+GET  /flights/fare?max=5000
+POST /flights  → { "flightNumber":"IG105", "source":"Mumbai", "destination":"Chennai", "departureTime":"16:00", "fare":4000, "availableSeats":60 }
+PUT  /flights/1
+DELETE /flights/1
+Passengers
+GET  /passengers
+POST /passengers → { "name":"Sneha", "email":"sneha@gmail.com", "phone":"9876543213" }
+PUT  /passengers/1
+DELETE /passengers/1
+Bookings
+GET /bookings
+POST /bookings?flightId=1&passengerId=1
+GET  /bookings/passenger/1
+GET  /bookings/flight/1
+PUT  /bookings/1/cancel
+DELETE /bookings/1*/
